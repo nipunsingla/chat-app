39272e00a06e78d93f7a7055608f6209fe89c223
Nipun <piyushsingla2416@gmail.com>
2020-01-01 17:45:59 +0530

    all done

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ io.on('connection', (socket) => {
         })
     })
 })
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
     console.log('running in ' + PORT);
 })
